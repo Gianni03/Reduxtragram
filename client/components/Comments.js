5602c83bec556d1a88f80fe9e5d02ef81465d6de
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2025-06-09 20:48:33 -0300

    displaying the single component

---
+++ client/components/Comments.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Comments = React.createClass({
+  render() {
+    return (
+
+      <div className="comment">
+        comments here!!
+      </div>
+    )
+  }
+})
+
+export default Comments;
